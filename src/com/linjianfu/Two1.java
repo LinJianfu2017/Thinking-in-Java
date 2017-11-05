97cd669ac94a085d527ddb64127217abb4a979cb
LinJianfu <1942305857@qq.com>
2017-11-05 18:15:52 +0800

    Initial commit

---
+++ src/com/linjianfu/Two1.java
@@ -0,0 +1,11 @@
+package com.linjianfu;
+
+public class Two1 {
+    static  int i;
+    static char c;
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("i="+i+"  c="+c);
+    }
+}
---
+++ src/com/linjianfu/Two2.java
@@ -0,0 +1,8 @@
+package com.linjianfu;
+
+public class Two2 {
+    public static void main(String[] arg){
+        System.out.printnb("Hello,World");
+    }
+
+}
