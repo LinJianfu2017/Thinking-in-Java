07a5f0c18866c4473929e1c8bce724ed720aed22
LinJianfu <1942305857@qq.com>
2017-12-01 14:17:28 +0800

    * 此处虽然调用基类构造器，但从结果来看，语句并没有实际的输出，似乎仅是一个声明，而并非执行语句。          * 从清理动作来看，反向结果也符合猜想，子类调用基类构造器的真正时机是发生于extends动作。

--- src/com/linjianfu/chapter7/Seven11.java
+++ src/com/linjianfu/chapter7/Seven11.java
@@ -1,32 +1,39 @@
 package com.linjianfu.chapter7;
 
 public class Seven11 {
-    private Cleanser c=new Cleanser();
-    public void append(String a){
+    private Cleanser c = new Cleanser();
+
+    public void append(String a) {
         c.append(a);
     }
-    public void dilute(){
+
+    public void dilute() {
         c.dilute();
     }
-    public void apply(){
+
+    public void apply() {
         c.apply();
     }
-    public void scrub(){
+
+    public void scrub() {
         append(" Seven11.scrub()");
         c.scrub();
     }
-    public void foam(){
+
+    public void foam() {
         append(" foam()");
     }
+
     @Override
-    public String toString(){
-       return c.toString();
+    public String toString() {
+        return c.toString();
     }
-/**
- * 通过代理的方式可实现选择性“继承”的目的，代理是介于组合和继承之间的一种状态。
- * */
+
+    /**
+     * 通过代理的方式可实现选择性“继承”的目的，代理是介于组合和继承之间的一种状态。
+     */
     public static void main(String[] args) {
-        Seven11 s11=new Seven11();
+        Seven11 s11 = new Seven11();
         s11.dilute();
         s11.apply();
         s11.scrub();
