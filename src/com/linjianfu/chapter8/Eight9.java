0c3e2060fb5db2a84416b21ebe1be4f8ce573ff7
LinJianfu <1942305857@qq.com>
2017-12-05 16:55:56 +0800

    dipose() final id(引用编号)/refcount(编号共享次数)/static counter(编号计数) finalize()

--- src/com/linjianfu/chapter8/Eight9.java
+++ src/com/linjianfu/chapter8/Eight9.java
@@ -4,18 +4,40 @@ import java.util.Random;
 
 //Exercise 9,12,14.
 class E9 {
+    private int refcount = 0;
+    private static long counter = 0;
+    private final long id = counter++;
+
     E9() {
-        System.out.println("E9()");
+        System.out.println("Creating" + this);
+    }
+
+    protected void addref14() {
+        refcount++;
+    }
+
+    protected void dispose14() {
+        if (--refcount == 0)
+            System.out.println("Disposing" + this);
+    }
+
+    protected void finalize() {
+        if (refcount > 0)
+            System.out.println("Error: " + refcount + this + " objects on use.");
+    }
+
+    @Override
+    public String toString() {
+        return " E9() No." + id;
     }
 }
 
 class Rodent {
-    protected String name = "Rodent";
-    private E9 r1 = new E9();
+    protected String name = " Rodent";
 
-    Rodent() {
-        System.out.println("Rodent()");
-    }
+//    Rodent() {
+//        System.out.println("Creating" + this);
+//    }
 
     protected void eat() {
         System.out.println(name + ".eat()");
@@ -36,45 +58,54 @@ class Rodent {
 }
 
 class Mouse extends Rodent {
-    private E9 m2 = new E9();
-
-    Mouse() {
-        System.out.println("Mouse()");
-    }
+//    private E9 e9 = new E9();
 
     {
-        super.name = "Mouse";
+        super.name = " Mouse";
     }
+
+
 }
 
 class Gerbil extends Rodent {
-    private E9 g3 = new E9();
-
-    Gerbil() {
-        System.out.println("Gerbil()");
-    }
+//    private E9 e9 = new E9();
 
     {
-        super.name = "Gerbil";
+        super.name = " Gerbil";
     }
 
 }
 
 class Hamster extends Rodent {
-    private E9 h4 = new E9();
+    //    private E9 e9 = new E9();
+    private static long counter = 0;
+    private final long id = counter++;
 
-    Hamster() {
-        System.out.println("Hamster()");
+    Hamster(E9 e9) {
+        super();
+        System.out.println("Creating" + this);
+        e9.addref14();
     }
 
     {
-        super.name = "Hamster";
+        super.name = " Hamster ";
+    }
+
+    protected void dispose(E9 e9) {
+        System.out.println("Disposing" + this);
+        e9.dispose14();
+    }
+
+    @Override
+    public String toString() {
+        return name + id;
     }
 
 }
 
 public class Eight9 {
     private Random rand = new Random(47);
+    static E9 e14 = new E9();
 
     private Rodent next() {
         switch (rand.nextInt(3)) {
@@ -84,11 +115,11 @@ public class Eight9 {
             case 1:
                 return new Gerbil();
             case 2:
-                return new Hamster();
+                return new Hamster(e14);
         }
     }
 
-    private static Rodent[] r = new Rodent[10];
+    private static Rodent[] r = new Rodent[3];
 
     public static void main(String[] args) {
         Eight9 e = new Eight9();
@@ -96,6 +127,21 @@ public class Eight9 {
             r[i] = e.next();
         for (Rodent o : r)
             System.out.println(o);
+        for (int i = 0; i < r.length; i++) {
+            if (r[i].name == " Hamster ")
+                ((Hamster) r[i]).dispose(e14);
+        }
+        e14.finalize();
+        Hamster h = new Hamster(e14);
+        e14.finalize();
+        E9 e142=new E9();
+        Hamster h2=new Hamster(e142);
+        Hamster h3=new Hamster(e142);
+        e142.finalize();
+        h2.dispose(e142);
+        h3.dispose(e142);
+        e142.finalize();
+
 //        int num = 0;
 //        switch (num) {
 //            case 0:
